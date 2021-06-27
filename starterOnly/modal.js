7a807b270ebbbd30d371b30989061d7d522b34c2
PROVILLE Maximilien <maximilien.proville77@gmail.com>
2021-06-27 20:19:19 +0200

    Projet4

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -81,10 +81,9 @@ function validate () {
   console.log(birthDate.value)
 
   if (!firstName.value.match(/(.*[a-z]){2}/i) || firstName.value == ' ' || firstName.value == null || firstName.value.length < 2) {
-    errorFirst.innerText = 'Deux caractères ou plus requis.';
+    errorFirst.innerText = 'At least two character require.';
     errorFirst.style.color = 'red';
     errorFirst.style.fontSize = '0.8rem';
-    errorFirst.style.marginTop = '10px';
     firstName.style.border = 'solid red 2px';
   } else {
     errorFirst.style.display = 'none';
@@ -93,10 +92,9 @@ function validate () {
   };
 
     if (!lastName.value.match(/(.*[a-z]){2}/i) || lastName.value == ' ' || lastName.value == null || lastName.value.length < 2) { 
-      errorLast.innerText = 'Deux caractères ou plus requis.';
+      errorLast.innerText = 'At least two character require.';
       errorLast.style.color = 'red';
       errorLast.style.fontSize = '0.8rem';
-      errorLast.style.marginTop = '10px';
       lastName.style.border = 'solid red 2px';      
   }  else {
     errorLast.style.display = 'none';
@@ -105,10 +103,9 @@ function validate () {
   };
 
     if (!/^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/.test(eMail.value)) { 
-      errorMail.innerText = 'Veuillez renseigner une addresse mail valide';
+      errorMail.innerText = 'Please enter a valid email address';
       errorMail.style.color = 'red';
       errorMail.style.fontSize = '0.8rem';
-      errorMail.style.marginTop = '10px';
       eMail.style.border = 'solid red 2px';
   } else {
     errorMail.style.display = 'none';
@@ -117,10 +114,9 @@ function validate () {
   };
 
   if (!birthDate.value.match(/^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/)) { 
-    errorBirth.innerText = 'Veuillez remplir votre date de naissance';
+    errorBirth.innerText = 'Please fill in your date of birth';
     errorBirth.style.color = 'red';
     errorBirth.style.fontSize = '0.8rem';
-    errorBirth.style.marginTop = '10px';
     birthDate.style.border = 'solid red 2px';
     } else {
     errorBirth.style.display = 'none';
@@ -129,10 +125,9 @@ function validate () {
     }
 
   if (!quantityTournament.value.match(numbers)) { 
-    errorQuantity.innerText = 'Vous devez indiquer un nombre valide';
+    errorQuantity.innerText = 'You must enter a valid number';
     errorQuantity.style.color = 'red';
     errorQuantity.style.fontSize = '0.8rem';
-    errorQuantity.style.marginTop = '10px';
     quantityTournament.style.border = 'solid red 2px';
   } else {
     errorQuantity.style.display = 'none';
@@ -141,20 +136,18 @@ function validate () {
   };
 
   if (!loc1.checked && !loc2.checked && !loc3.checked && !loc4.checked && !loc5.checked && !loc6.checked) { 
-    errorCity.innerText = 'Vous devez choisir une ville';
+    errorCity.innerText = 'A city must be chosen';
     errorCity.style.color = 'red';
-    errorCity.style.fontSize = '0.8rem';
-    errorCity.style.marginTop = '10px';          
+    errorCity.style.fontSize = '0.8rem';          
   } else {
     errorCity.style.display = 'none';
     radioChecked = true;
   };
 
   if (!validation.checked) {
-    errorValidation.innerText = 'Les termes et conditions doivent etre acceptés';
+    errorValidation.innerText = 'Terms and condition must be accepted';
     errorValidation.style.color = 'red';
     errorValidation.style.fontSize = '0.8rem';
-    errorValidation.style.marginTop = '10px';
     errorValidation.style.marginBottom = '20px';
   } else {
     errorValidation.style.display = 'none';
